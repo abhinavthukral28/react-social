88997225217d09cf9f5038560bfd03feb5ed29be
Abhinav Thukral <abhinav@abhinavthukral.com>
2020-06-26 21:02:05 +0530

    Login Working

--- functions/index.js
+++ functions/index.js
@@ -50,9 +50,32 @@ app.post("/screams", (request, response) => {
       response.status(500).json({ error: "Something went wrong" });
     });
 });
+const isEmpty = (string) => {
+  return string.trim() === "" ? true : false;
+};
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return email.match(emailRegEx);
+};
 app.post("/signup", async (request, response) => {
-  const token = null;
+  let errors = {};
   const newUser = { ...request.body };
+  if (isEmpty(newUser.email)) {
+    errors.email = "Email is empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Not a valid email";
+  }
+  if (isEmpty(newUser.password)) {
+    errors.password = "Password is empty";
+  } else if (newUser.password !== newUser.confirmedPassword) {
+    errors.confirmPassword = "passwords do not match";
+  }
+  if (isEmpty(newUser.handle)) {
+    errors.handle = "Handle is empty";
+  }
+  if (Object.keys(errors).length > 0) {
+    return response.status(400).json(errors);
+  }
   let userRef = null;
   try {
     userRef = await db.collection("users").doc(newUser.handle).get();
@@ -72,13 +95,36 @@ app.post("/signup", async (request, response) => {
       const addUser = await db
         .doc(`users/${newUser.handle}`)
         .set(userCredentials);
+      const token = await addUserRequest.user.getIdToken();
 
       return response.status(201).json({
-        message: `user ${addUserRequest.user.uid} created successfully`,
+        token: token,
       });
     }
   } catch (err) {
     response.status(500).json(err);
+  }
+});
+
+app.post("/login", async (request, response) => {
+  let errors = {};
+  const user = { ...request.body };
+  if (isEmpty(user.email)) errors.email = "Email cannot be empty";
+  else if (!isEmail(user.email)) errors.email = "Invalid Email";
+  if (isEmpty(user.password)) errors.password = "Password cannot be empty";
+  if (Object.keys(errors).length > 0) return response.status(400).json(errors);
+  else {
+    try {
+      let login = await firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password);
+
+      const token = await login.user.getIdToken();
+
+      return response.status(200).json({ token });
+    } catch (err) {
+      return response.status(500).json(err);
+    }
   }
 });
 
